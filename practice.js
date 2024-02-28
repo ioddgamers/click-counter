9d029740836021adb4c2997fd9df9217f38c03bb
Tolulope <toluloperbrts@gmail.com>
2024-02-28 18:49:37 +0100

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,23 @@
+const counter = document.querySelector("p");
+const decreaseValue = document.getElementById("decrease");
+const resetValue = document.getElementById("reset");
+const increaseValue = document.getElementById("increase");
+let counterValue = 0;
+  
+function decreaseCounterValue () {
+  counter.textContent = counterValue--;
+}  
+
+function resetCounterValue () {
+  counterValue = 0;
+  counter.textContent = counterValue;
+}
+
+function increaseCounterValue () {
+  counter.textContent = counterValue++;
+}
+
+
+decreaseValue.addEventListener("click", decreaseCounterValue);
+resetValue.addEventListener("click", resetCounterValue);
+increaseValue.addEventListener("click", increaseCounterValue);
